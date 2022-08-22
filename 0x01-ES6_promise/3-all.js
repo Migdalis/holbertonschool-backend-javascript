d19856501d06e10e51f8676cc0bab97fd8e1d851
Migdalis <miglepage@gmail.com>
2022-08-22 17:23:23 -0500

    :white_check_mark: Fix task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
-      const body = response[0];
+      const { body } = response[0];
       const { firstName, lastName } = response[1];
       console.log(`${body} ${firstName} ${lastName}`);
     })
